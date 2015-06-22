3acc5c151972229bfd19a08a06690b81ef2bfc55
mesuyog <suyog844@gmail.com>
2015-06-22 23:32:06 +0545

    Update vimeo.py

--- vimeo.py
+++ vimeo.py
@@ -7,11 +7,5 @@ V = vimeo.VimeoClient(
     key="b7423b6eaf753a63054398eb1266000a9ea10953",
     secret="Xx+B7Yr8LMia4zFSu8uVTyOSTb63Ljs0P8BgptQu8ZuxTgNKicIlU/1xZk2YUk+4dI4bIM0v8DLWt5FgpLwyGUT+E0dafhKo+4aMyhNmu5yb9R9po6hZam/2T1c1pr8V"
 )
-about_me = V.get('https://vimeo.com/home/myvideos')
-
-assert about_me.status_code == 200
-# print about_me.json()
-
-vimeo_authorization_url = V.auth_url(['public'], ['private'], 'https:example.com')
 
 video_uri = V.upload('C:\Users\Sakuragi-Kun\Desktop\my.mp4')
